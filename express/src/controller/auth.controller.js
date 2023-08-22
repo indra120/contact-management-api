a55dcda6babc042c720e51d8832c89fd405d5b94
Indra Zen <indrapranata120@gmail.com>
2023-08-22 21:06:46 +0800

    Create login method in auth controller

--- express/src/controller/auth.controller.js
+++ express/src/controller/auth.controller.js
@@ -8,3 +8,12 @@ export async function register(req, res, next) {
     next(e)
   }
 }
+
+export async function login(req, res, next) {
+  try {
+    const data = await service.login(req.body)
+    res.status(200).json({data})
+  } catch (e) {
+    next(e)
+  }
+}
