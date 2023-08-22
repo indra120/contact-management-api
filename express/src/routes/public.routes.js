5efd1638e17cefbd7ec7f6a4b0f647366c911644
Indra Zen <indrapranata120@gmail.com>
2023-08-22 21:09:26 +0800

    Assign login method to public routes

--- express/src/routes/public.routes.js
+++ express/src/routes/public.routes.js
@@ -4,5 +4,6 @@ import * as authController from '#controller/auth'
 const router = Router()
 
 router.post('/register', authController.register)
+router.post('/login', authController.login)
 
 export { router as publicRoutes }
