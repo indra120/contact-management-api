28391ba8c198d51e9724f2fa749e62b007fecd5f
Indra Zen <indrapranata120@gmail.com>
2023-08-23 14:11:42 +0800

    Update-user validation

--- express/src/validation/user.validation.js
+++ express/src/validation/user.validation.js
@@ -1,3 +1,9 @@
 import Joi from 'joi'
 
 export const getUserValidation = Joi.string().max(100).required()
+
+export const updateUserValidation = Joi.object({
+  username: Joi.string().max(100).required(),
+  password: Joi.string().max(100).optional(),
+  name: Joi.string().max(100).optional(),
+})
