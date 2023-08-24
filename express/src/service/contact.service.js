0771988c5bf4ec79eb48f7f2a05ede5ede44c684
Indra Zen <indrapranata120@gmail.com>
2023-08-24 19:29:05 +0800

    Add create method in contact-service

---
+++ express/src/service/contact.service.js
@@ -0,0 +1,20 @@
+import { db } from '#lib/db'
+import { validate } from '#lib/validation'
+import { createContactValidation } from '#validation/contact'
+
+export async function create(user, request) {
+  const data = validate(createContactValidation, request)
+
+  data.username = user.username
+
+  return db.data.create({
+    data,
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      phone: true,
+    },
+  })
+}
